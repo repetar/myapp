96360d6e098e3fc1a7daf85ee333eed6a3a7b4f8
epetato <petar.tomic@ericsson.com>
2020-06-17 18:16:17 +0000

    database client

--- source/traffic-handler/src/main/java/my/project/app/traffichandler/TrafficHandler.java
+++ source/traffic-handler/src/main/java/my/project/app/traffichandler/TrafficHandler.java
@@ -1,6 +1,106 @@
 package my.project.app.traffichandler;
 
+import com.mongodb.*;
+import com.mongodb.DBCollection;
+
+import java.net.InetAddress;
+
+import com.mongodb.client.MongoDatabase;
+import my.project.app.traffichandler.databaseobjects.TestObject;
+
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class TrafficHandler {
 
+    private static DBObject createDBObject(TestObject to) {
+        BasicDBObjectBuilder docBuilder = BasicDBObjectBuilder.start();
+
+        docBuilder.append("att1", to.getAtt1());
+        docBuilder.append("att2", to.getAtt2());
+        docBuilder.append("att3", to.getAtt3());
+        docBuilder.append("att4", to.isAtt4());
+        return docBuilder.get();
+    }
+
+    public static void main(final String[] args) {
+
+        System.out.println("Hello!!!!!!!!!!!!!!!!!");
+        InetAddress[] mongoPods = null;
+
+
+        try {
+             mongoPods = InetAddress.getAllByName("example-mongodb-svc");
+
+        } catch (UnknownHostException e) {
+            System.out.println("Exception while getting mongo pods:" + e);
+
+        }
+
+        List list = new ArrayList();
+
+        for (InetAddress address : mongoPods) {
+            //mongoAddresses.add(address.getHostAddress());
+            list.add(new ServerAddress(address.getHostAddress(), 27017));
+
+        }
+
+
+        MongoClient mongoClient = new MongoClient(list);
+        DB db = mongoClient.getDB("mytestdb");
+        TestObject to = new TestObject(1, 2, "teststring", true);
+
+
+        // create
+        System.out.println("### CERATE");
+        DBCollection col = db.getCollection("testcollection");
+        DBObject dbo = createDBObject(to);
+
+        WriteResult result = col.insert(dbo);
+        System.out.println(result.getUpsertedId());
+        System.out.println(result.getN());
+        System.out.println(result.isUpdateOfExisting());
+        System.out.println(result.wasAcknowledged());
+
+
+        // read
+        System.out.println("### READ");
+        DBObject query = BasicDBObjectBuilder.start().add("att1", to.getAtt1()).get();
+        DBCursor cursor = col.find(query);
+        while(cursor.hasNext()){
+            System.out.println(cursor.next());
+        }
+
+        //update example
+        System.out.println("### UPDATE");
+        to.setAtt1(6554);
+        dbo = createDBObject(to);
+        result = col.update(query, dbo);
+        System.out.println(result.getUpsertedId());
+        System.out.println(result.getN());
+        System.out.println(result.isUpdateOfExisting());
+        System.out.println(result.wasAcknowledged());
+
+        //delete example
+        System.out.println("### DELETE");
+        result = col.remove(query);
+        System.out.println(result.getUpsertedId());
+        System.out.println(result.getN());
+        System.out.println(result.isUpdateOfExisting());
+        System.out.println(result.wasAcknowledged());
+
+        //close resources
+        mongoClient.close();
+
+
+        while(true) {
+            //
+
+        }
+
+    }
 
 }
---
+++ source/traffic-handler/src/main/java/my/project/app/traffichandler/databaseobjects/TestObject.java
@@ -0,0 +1,51 @@
+package my.project.app.traffichandler.databaseobjects;
+
+public class TestObject {
+
+    private int att1;
+
+    private int att2;
+
+    private String att3;
+
+    private boolean att4;
+
+    public TestObject ( int att1, int att2, String att3, boolean att4) {
+        this.att1 = att1;
+        this.att2 = att2;
+        this.att3 = att3;
+        this.att4 = att4;
+    }
+
+    public int getAtt1() {
+        return att1;
+    }
+
+    public void setAtt1(int att1) {
+        this.att1 = att1;
+    }
+
+    public int getAtt2() {
+        return att2;
+    }
+
+    public void setAtt2(int att2) {
+        this.att2 = att2;
+    }
+
+    public String getAtt3() {
+        return att3;
+    }
+
+    public void setAtt3(String att3) {
+        this.att3 = att3;
+    }
+
+    public boolean isAtt4() {
+        return att4;
+    }
+
+    public void setAtt4(boolean att4) {
+        this.att4 = att4;
+    }
+}
