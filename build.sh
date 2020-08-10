9b5c55dcce55f25822ed3989f44bf996d1a3be13
epetato <petar.tomic@ericsson.com>
2020-08-10 21:00:22 +0000

    final changes

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ function main() {
 
   docker build -t my.project.app.requesthandler:1.0.0 source/request-handler/ --no-cache
 
-  docker build -t fe-app:0.0.1 source/front-end/fe-app/
+  docker build -t fe-app:0.0.2 source/front-end/fe-app/ --no-cache
 
 }
 
